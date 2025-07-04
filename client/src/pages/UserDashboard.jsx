71bb88144c6f949a0e963c5e86eac8fd93bbeb8b
MansiChache <crce.9949.ce@gmail.com>
2025-07-04 18:48:52 +0530

    UserDashboardfinalized UI

--- client/src/pages/UserDashboard.jsx
+++ client/src/pages/UserDashboard.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import TicketCard from "../components/TicketCard";
+import Footer from '../components/Footer';
 
 const UserDashboard = () => {
   return (
@@ -8,27 +9,28 @@ const UserDashboard = () => {
       <Navbar />
       <Sidebar />
 
-      <main className="pt-[70px] pl-[250px] min-h-screen bg-white">
+      <main className="pt-[50px] pl-[250px] min-h-screen bg-white">
         <div className="p-8">
-          <h2 className="text-center text-lg font-semibold mb-8">
+          <h2 className="text-center text-3xl font-sanchez mb-2  ">
             Dashboard
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
             <TicketCard
               title="Total Tickets"
               count={12}
-              bgColor="#3066BE"
+              bgColor="#296EF2"
             />
-            <TicketCard title="Total Solved" count={8} bgColor="#2AF5FF" />
+            <TicketCard title="Total Solved" count={8} bgColor="#0BFF68" />
             <TicketCard
-              title="Awaiting Approval"
+              title="Total Awaiting Approval"
               count={2}
-              bgColor="red"
+              bgColor="#FE594E"
             />
-            <TicketCard title="In Progress" count={2} bgColor="#F5E960" />
+            <TicketCard title="Total In Progress" count={2} bgColor="#FCFF6C" />
           </div>
         </div>
       </main>
+      <Footer/>
     </div>
   );
 };
